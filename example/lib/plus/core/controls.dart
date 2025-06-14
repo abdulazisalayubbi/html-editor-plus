9de0d24d99615228a3b1842a6a347dea5c3cc9d7
abdulazisalayubbi <azisa6980@gmail.com>
2025-06-14 13:39:39 +0700

    added margin 16

--- example/lib/plus/core/controls.dart
+++ example/lib/plus/core/controls.dart
@@ -9,7 +9,7 @@ class EditorControls extends StatelessWidget {
   Widget build(BuildContext context) => ConstrainedBox(
         constraints: const BoxConstraints(maxWidth: 400),
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(16.0),
           child: Wrap(
             alignment: WrapAlignment.center,
             runAlignment: WrapAlignment.center,
